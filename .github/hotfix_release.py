bb9dc91683909d854f8f52f7f62ecd750d2eaa40
Vivian Fan <88053079+vivian-fan@users.noreply.github.com>
2022-02-09 14:43:39 -0800

    Update hotfix_release.py

--- .github/hotfix_release.py
+++ .github/hotfix_release.py
@@ -1,7 +1,7 @@
 # Goal: recompute versions on develop and master branch
 # return {'include': [{'file': 'internal', 'version': 1.1.0, 'branch': 'master'}, {'file': 'external', 'version': 1.1.0, 'branch': 'develop'}, {'file': 'data_science', 'version': 1.1.0, 'branch': 'master'}]}
 
-version_matrix = = {'include': []}
+version_matrix = {'include': []}
 
 # Get latest_release_version
 latest_release_version = ?
@@ -13,6 +13,17 @@ latest_release_version = ?
 #   intent_list from .github/intent.yml from develop branch
 #   do a loop to calculate new versions
 # Append result behind and set branch as 'develop'
+
+# intent_list from .github/intent.yml 
+# {'external': ['major', 'minor'], 'external': ['minor', 'minor'], 'data_science': ['major']}
+# return {'external': 'major', 'external': 'minor', 'data_science': 'major'}
+
+intent_dic = {'external': 'major', 'external': 'minor', 'data_science': 'major'}
+for file in intent_dic:
+  file_name = file + '.yml'
+  target_version = get_target_version(target_branch, file_name)
+  new_version = calculate_version(intent_dic[file], target_version, latest_release_version)
+  version_matrix['include'].append({'file': file_name, 'version': new_version, 'branch': target branch})
 
 # Get new version on master branch
 # params:
