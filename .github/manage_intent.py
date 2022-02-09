89f8739f69d766340b452f564a99ecf60accbf82
Vivian Fan <88053079+vivian-fan@users.noreply.github.com>
2022-02-09 14:54:07 -0800

    Update manage_intent.py

--- .github/manage_intent.py
+++ .github/manage_intent.py
@@ -1,1 +1,9 @@
+# Get intent from intent.yml on feature branch
 
+# Get intent_list from .github/intent.yml on master branch
+
+# Append the intent in intent_list
+
+# Update the intent_list into .github/intent.yml on master branch
+
+# Push the change back to master branch
