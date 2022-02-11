631c62ced2930f19cf946179bb06f6aac91c2cca
Vivian Fan <88053079+vivian-fan@users.noreply.github.com>
2022-02-11 10:57:56 -0800

    Update manage_intent.py

--- .github/manage_intent.py
+++ .github/manage_intent.py
@@ -6,11 +6,13 @@ import yaml
 import git
 import json
 
+
 def get_remote():
     username = "vivian-fan"
     password = sys.argv[1]
     remote = f"https://{username}:{password}@github.com/vivian-fan/hotfix_release_version.git"
     return remote
+
 
 def get_clone_repo(remote, path, branch):
     if os.path.exists(path):
@@ -19,27 +21,31 @@ def get_clone_repo(remote, path, branch):
     clone_repo = git.Repo.clone_from(remote, path, branch=branch)
     return clone_repo
 
+
 def get_intents(path):
     with open(path + "/.github/intent.yml", "r") as intent_mgmt_file:
         intent_mgmt_content = yaml.safe_load(intent_mgmt_file)
     return intent_mgmt_content
 
+
 def get_intent(path):
-     with open(path + "/intent.yml", "r") as intent_file:
+    with open(path + "/intent.yml", "r") as intent_file:
         intent_content = yaml.safe_load(intent_file)
     return intent_content
 
+
 def push_to_origin(intent_mgmt_content, target_path, target_branch):
-    with open(target_path + '/.github/intent.yml', 'w') as intent_mgmt_file:
+    with open(target_path + "/.github/intent.yml", "w") as intent_mgmt_file:
         intent_mgmt_file.seek(0)
-        intent_mgmt_file.write( yaml.dump(intent_mgmt_content, default_flow_style=False))
+        intent_mgmt_file.write(yaml.dump(intent_mgmt_content, default_flow_style=False))
     try:
         repo = git.Repo(target_path)
         repo.git.add(update=True)
-        repo.index.commit('add intent to ' + target_branch)
+        repo.index.commit("add intent to " + target_branch)
         repo.git.push("origin", target_branch)
     except Exception as e:
-        print('Errors occured while pushing the code', e) 
+        print("Errors occured while pushing the code", e)
+
 
 remote = get_remote()
 # Clone master branch
@@ -75,18 +81,25 @@ released_intents = get_intents(release_path)
 
 print(
     "Before: ",
-    "master:", master_intents, "develop:", dev_intents, "released: ", released_intents
+    "master:",
+    master_intents,
+    "develop:",
+    dev_intents,
+    "released: ",
+    released_intents,
 )
 
 # If the event is PR merge, add hotfix to release
 event = sys.argv[1]
-if event == 'pull_request':
+if event == "pull_request":
     hotfix_branch = sys.argv[2]
     hotfix_path = "./hotfix"
     clone_repo_hotfix = get_clone_repo(remote, hotfix_path, hotfix_branch)
     hotfix_intent = get_intent(hotfix_path)
-    for file in hotfix_intent['intent']:
-        released_intents['intent'][file].append({'id': hotfix_branch, 'intent': hotfix_intent['intent'][file]})
+    for file in hotfix_intent["intent"]:
+        released_intents["intent"][file].append(
+            {"id": hotfix_branch, "intent": hotfix_intent["intent"][file]}
+        )
 
 # # Delete released_intents from master_intents
 # for file in released_intents['intent']:
@@ -94,7 +107,7 @@ if event == 'pull_request':
 #         for intent_dic in released_intents['intent'][file]:
 #             if intent_dic in master_intents['intent'][file]:
 #                 master_intents['intent'][file].remove(intent_dic)
-                
+
 # push_to_origin(master_intents, master_path, 'master')
 
 # # Delete released_intents from dev_intents
@@ -103,16 +116,21 @@ if event == 'pull_request':
 #         for intent_dic in released_intents['intent'][file]:
 #             if intent_dic in dev_intents['intent'][file]:
 #                 dev_intents['intent'][file].remove(intent_dic)
-                
+
 # push_to_origin(dev_intents, dev_path, 'develop')
-                
+
 # # Delete released_intents from dev_intents
 # for file in released_intents['intent']:
 #     released_intents['intent'][file] = []
-    
+
 # push_to_origin(released_intents, release_path, latest_release_branch)
 
 print(
     "After: ",
-    "master:", master_intents, "develop:", dev_intents, "released: ", released_intents
+    "master:",
+    master_intents,
+    "develop:",
+    dev_intents,
+    "released: ",
+    released_intents,
 )
