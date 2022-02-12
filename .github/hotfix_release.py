ca83f7585f42c169cae095b2cdb021209a821ab3
Vivian Fan <88053079+vivian-fan@users.noreply.github.com>
2022-02-11 16:58:30 -0800

    Update hotfix_release.py

--- .github/hotfix_release.py
+++ .github/hotfix_release.py
@@ -78,12 +78,13 @@ def recalculate_version(version_matrix, intent_dic, target_branch):
 
 def combine_intent(master_intents):
     intent_dic = {}
-    for file in master_intents["intent"] and len(master_intents["intent"][file]) != 0:
-        intent = minor
-        for intent_item in master_intents["intent"][file]:
-            if intent_item["intent"] == "major":
-                intent = "major"
-        intent_dic[file] = intent
+    for file in master_intents["intent"]:
+        if len(master_intents["intent"][file]) != 0:
+            intent = minor
+            for intent_item in master_intents["intent"][file]:
+                if intent_item["intent"] == "major":
+                    intent = "major"
+            intent_dic[file] = intent
     return intent_dic
 
 
